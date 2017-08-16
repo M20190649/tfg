082527bb64200ab82d1ee4f73b252269043b1007
Adrián Álvarez Sáez <adalsa91@gmail.com>
2017-08-17 01:14:51 +0200

    Closes #7

--- lib/sniffer/sniffer.cpp
+++ lib/sniffer/sniffer.cpp
@@ -1,6 +1,17 @@
 #include "sniffer.hpp"
-#include <stdio.h>
 #include "Arduino.h"
+#include <stdio.h>
+#include <string>
+
+MD5Builder _md5;
+
+String md5(String str) {
+    _md5.begin();
+    _md5.add(String(str));
+    _md5.calculate();
+
+    return _md5.toString();
+}
 
 /**
  * Callback for promiscuous mode
@@ -18,7 +29,7 @@ void ICACHE_FLASH_ATTR sniffer_callback(uint8_t *buffer, uint16_t length)
               char mac_addr[18];
               sprintf(mac_addr, "%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
               Serial.println();
-              Serial.print(mac_addr);
+              Serial.print(md5(mac_addr));
         }
     }
 }
