cbe04222fd10be5382acaaa0e9a9e7fce00c686f
Adrián Álvarez Sáez <adalsa91@gmail.com>
2017-08-17 19:58:17 +0200

    Closes #10

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,14 @@
 // Expose Espressif SDK functionality
 extern "C" {
     #include "user_interface.h"
+    #include "sntp.h"
 }
 
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <sniffer.hpp>
+#include <Time.h>
+
 
 #define CHANNEL_HOP_INTERVAL_MS   1000 // We will change WiFi channel every second
 static os_timer_t channel_hop_timer; // Software timer for channel surgfing
@@ -24,6 +28,38 @@ void setup()
     Serial.println();
     Serial.println("Initializing system.");
 
+    // WiFi connection
+    WiFi.begin("SSID", "PASSWORD");
+    Serial.print("Connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+
+    //Get time through SNTP
+    Serial.print("Getting timestamp from server.");
+    sntp_setservername(0, "hora.roa.es");
+    sntp_setservername(1, "pool.ntp.org");
+    sntp_setservername(2, "time.nist.gov");
+    sntp_set_timezone(+2);
+    sntp_init();
+
+    uint32_t current_stamp = 0;
+    while (current_stamp == 0) {
+        current_stamp = sntp_get_current_timestamp();
+        delay(100);
+        Serial.print(".");
+    }
+    setTime(current_stamp);
+    sntp_stop();
+
+    Serial.printf("\nSNTP OK\nTimestamp:	%d,	%s	\n",current_stamp, sntp_get_real_time(current_stamp));    
+
+    // Promiscuous mode
     wifi_set_opmode(STATION_MODE); // Promiscuous mode only works with station mode
     wifi_station_disconnect(); // API requirement before enable promiscuous mode.
     wifi_set_channel(1);
